779a8106737eb2014b1f9d558c5d5758e25e5431
kdchang <kdchang.cc@gmail.com>
2025-05-09 19:06:28 +0800

    feat: update auth url tail slash

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -5,10 +5,10 @@ import axios from '../axios';
 export const useAuthStore = defineStore('auth', {
   actions: {
     async login(email, password) {
-      await axios.post('/auth/login/', { email, password });
+      await axios.post('/auth/login', { email, password });
     },
     async signup(firstname, lastname, username, email, password) {
-      await axios.post('/auth/signup/', {
+      await axios.post('/auth/signup', {
         firstname,
         lastname,
         username,
@@ -17,7 +17,7 @@ export const useAuthStore = defineStore('auth', {
       });
     },
     async logout() {
-      await axios.post('/auth/logout/');
+      await axios.post('/auth/logout');
     },
   },
 });
