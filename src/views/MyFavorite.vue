aa2f280f1d634e2c1706427a210c25eafe04b31f
kdchang <kdchang.cc@gmail.com>
2025-05-21 17:44:34 +0800

    feat: add my favorites page

---
+++ src/views/MyFavorite.vue
@@ -0,0 +1,36 @@
+<template>
+  <Navbar></Navbar>
+  <div class="p-6 min-h-screen">
+    <h1 class="text-2xl font-bold mb-4">我的收藏</h1>
+
+    <div v-if="restaurants.length === 0">尚未有收藏</div>
+    <div v-else class="grid gap-4">
+      <RestaurantCard
+        v-for="r in restaurants"
+        :key="r.placeId"
+        :restaurant="r"
+      />
+    </div>
+  </div>
+  <Footer></Footer>
+</template>
+
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
+import RestaurantCard from '@/components/RestaurantCard.vue';
+
+import axios from '@/axios';
+import { ref, onMounted } from 'vue';
+
+const restaurants = ref([]);
+
+onMounted(async () => {
+  try {
+    const res = await axios.get('/favorites/');
+    restaurants.value = res.data.restaurants;
+  } catch (err) {
+    alert(err);
+  }
+});
+</script>
