8a488cdb865915021407c4fbda2783100b062414
Jae <comea22@gmail.com>
2025-05-21 17:29:49 +0800

    feat:refine restaurants list

--- src/views/RestaurantsList.vue
+++ src/views/RestaurantsList.vue
@@ -13,16 +13,7 @@
             >
             
             <RestaurantCard :restaurant="r" />
-              <!-- <router-link :to="`/restaurants/${r.placeId}`" class="flex flex-row gap-4 w-full items-start">
-                
-                <div class="flex flex-col flex-1">
-                  <div class="flex flex-row justify-between items-center">
-                    <p class="inline-block  whitespace-nowrap truncate max-w-[8em] sm:max-w-[10em] md:max-w-[12em] font-bold text-black text-xs sm:text-base ">{{ r.name }}</p>
-                  </div>
-                  <p class="text-black text-xs sm:text-base">⭐ {{ r.googleRating }}</p>
-                  <p class="inline-block truncate whitespace-nowrap  max-w-[8em] sm:max-w-[11em] md:max-w-[13em]  text-black text-xs sm:text-base ">{{ r.address }}</p>
-                </div>
-              </router-link> --> 
+            
             </li>
           </ul>
 
@@ -42,39 +33,7 @@ import { useRestaurantStore } from '@/stores/restaurant';
 const store = useRestaurantStore();
 const topTenRestaurants = computed(() => store.restaurants.slice(0, 12));
 
-// const restaurants = ref([
-//   {
-//     image: 'https://hips.hearstapps.com/hmg-prod/images/1-1525230190.jpg',
-//     name: '王媽媽手作古早味',
-//     rating: 4.5,
-//     address: '新北市板橋區文化路一段100號之6，頂樓B室',
-//   },
-//   {
-//     image:
-//       'https://www.xianlaoman.com.tw/upload/products/20231101194532y2hnf.jpg',
-//     name: '老王牛肉麵',
-//     rating: 4.7,
-//     address: '台北市大安區信義路三段100號',
-//   },
-//   {
-//     image: 'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38',
-//     name: '阿強排骨飯',
-//     rating: 4.6,
-//     address: '台北市中山區南京東路三段88號',
-//   },
-//   {
-//     image: 'https://images.unsplash.com/photo-1540189549336-e6e99c3679fe',
-//     name: '小林臭豆腐',
-//     rating: 4.3,
-//     address: '新北市板橋區中山路二段222號',
-//   },
-//   {
-//     image: 'https://images.unsplash.com/photo-1570197781559-4e8e54b1c9cd',
-//     name: '大戶屋日式定食',
-//     rating: 4.8,
-//     address: '台中市西區美村路一段123號',
-//   },
-// ]);
+
 </script>
 
 <style scoped>
