c01c1eb4ee6410bc6390ecddeb56e348a0c5e52e
panda <qWwe79790922@gmail.com>
2025-05-29 15:10:36 +0800

    feat: create Slogan component for homepage

---
+++ src/components/Slogan.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-secondary px-6 py-10">
+    <div
+      class="max-w-5xl mx-auto flex flex-col md:flex-row items-center justify-between gap-6"
+    >
+      <!-- 左邊：大標 -->
+      <div
+        class="text-4xl font-bold text-center md:text-left leading-relaxed text-primary"
+      >
+        <p>下一餐？</p>
+        <p>我們比你還急。</p>
+      </div>
+
+      <!-- 右邊：內文說明 -->
+      <div
+        class="text-base text-center md:text-left leading-relaxed text-[color:rgb(87,57,33)]"
+      >
+        <p>
+          EatHub 尋飽 是一個專為選擇困難症設計的美食推薦平台。<br />
+          根據你的口味偏好與所在地位置，快速篩選出專屬於你的美食選項。<br />
+          不用再煩惱「下一餐吃什麼」，讓我們幫你尋找最對味的那一口，輕鬆飽餐一頓！
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
